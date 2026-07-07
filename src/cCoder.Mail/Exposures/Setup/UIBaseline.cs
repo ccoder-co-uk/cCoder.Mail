fa0c9e89fefbe4d1d642c5c768ad653e5f68e31b
Paul Ward <paul.ward@ccoder.co.uk>
2026-07-07 14:27:59 +0100

    Add Mail baseline endpoint (#17)
    * Add Mail baseline endpoint
    
    * Add UI baseline package payloads
    
    * Split UI baseline packages into partials

---
+++ src/cCoder.Mail/Exposures/Setup/UIBaseline.cs
@@ -0,0 +1,10 @@
+using cCoder.Data.Models.Packaging;
+
+namespace cCoder.Mail.Exposures.Setup;
+
+public static partial class UIBaseline
+{
+    public static Package[] Packages => [
+        Components
+    ];
+}
