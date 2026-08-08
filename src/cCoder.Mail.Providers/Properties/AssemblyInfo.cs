b5d55823ba4361b3e272b451c399f3f5194e13d4
TehWardy <paul.ward@ccoder.co.uk>
2026-08-08 16:56:57 +0100

    test: enforce service and exposure coverage

--- src/cCoder.Mail.Providers/Properties/AssemblyInfo.cs
+++ src/cCoder.Mail.Providers/Properties/AssemblyInfo.cs
@@ -6,3 +6,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("cCoder.Mail.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("ProxyBuilder")]
