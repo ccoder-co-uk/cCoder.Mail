a4ea88ce904d317f6fe0d0e0cae138f387e1c31d
TehWardy <paul.ward@ccoder.co.uk>
2026-09-13 12:20:44 +0100

    Cover Mail foundation dependency handling

--- src/cCoder.Mail.Tests/Foundations/MailSendingServiceTests.cs
+++ src/cCoder.Mail.Tests/Foundations/MailSendingServiceTests.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Paul.Ward@ccoder.co.uk
 // ---------------------------------------------------------------
 
+#pragma warning disable STXTEST005
+
 using cCoder.Mail.Brokers.MailClients;
 using cCoder.Mail.Brokers.Loggings;
 using cCoder.Mail.Exposures;
@@ -13,14 +15,20 @@ namespace cCoder.Core.Services.Tests.Mail.Foundations;
 public partial class MailSendingServiceTests
 {
     private readonly Mock<IMailSenderClientBroker> mailSenderClientBrokerMock;
+    private readonly Mock<IMailConfigurationExposure> mailConfigurationExposureMock;
+    private readonly Mock<ILoggingBroker> loggerMock;
     private readonly MailSendingService mailSendingService;
 
     public MailSendingServiceTests()
     {
         mailSenderClientBrokerMock = new Mock<IMailSenderClientBroker>(behavior: MockBehavior.Strict);
+        mailConfigurationExposureMock = new Mock<IMailConfigurationExposure>(behavior: MockBehavior.Strict);
+        loggerMock = new Mock<ILoggingBroker>(behavior: MockBehavior.Strict);
         mailSendingService = new MailSendingService(
             mailSenderClientBroker: mailSenderClientBrokerMock.Object,
-            mailConfigurationExposure: Mock.Of<IMailConfigurationExposure>(),
-            logger: Mock.Of<ILoggingBroker>());
+            mailConfigurationExposure: mailConfigurationExposureMock.Object,
+            logger: loggerMock.Object);
     }
 }
+
+#pragma warning restore STXTEST005
